93a34147f08fb2e6378d239b286a3b6033b79936
Sergiy Kyrylenko <sergiy.kyrylenko@travisperkins.co.uk>
2017-04-04 20:09:59 +0300

    without rbf

---
+++ my1.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity my1_top is
+	port(
+		CLK_20			: in std_logic;
+		CLK_20_ALT		: in std_logic
+	);
+  
+end my1_top;
+
+architecture rtl of my1_top is
+
+begin
+
+end rtl;
